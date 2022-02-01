754a9f36fd56ead7eaa2537e68d29fd4714d4f20
Danilo Marques <danilomarques20@hotmail.com>
2022-01-31 21:04:04 -0300

    adding heroku base url.

--- cmd/service/master.go
+++ cmd/service/master.go
@@ -10,7 +10,7 @@ import (
 	"github.com/danilomarques1/gopm/cmd/util"
 )
 
-const BASE_URL = "http://127.0.0.1:8080"
+const BASE_URL = "https://gopmserver.herokuapp.com"
 
 type MasterService struct {
 	client *http.Client
