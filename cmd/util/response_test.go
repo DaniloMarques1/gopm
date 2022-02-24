90f82c2a26f249e9a8eacb57d94a75b3c20f8b3f
Danilo Marques <danilomarques20@hotmail.com>
2022-02-23 21:19:31 -0300

    adding util tests.

---
+++ cmd/util/response_test.go
@@ -0,0 +1,34 @@
+package util
+
+import (
+	"io"
+	"strings"
+	"testing"
+)
+
+const (
+	ERROR_RESPONSE = `{"message": "Some crazy shit error"}`
+)
+
+func mockReader(str string) io.Reader {
+	return strings.NewReader(str)
+}
+
+func TestHandleError(t *testing.T) {
+	cases := []struct {
+		label       string
+		reader      io.Reader
+		message     string
+		returnedErr bool
+	}{
+		{"ShouldParseError", mockReader(ERROR_RESPONSE), "Some crazy shit error", false},
+	}
+
+	for _, tc := range cases {
+		err := HandleError(tc.reader)
+		if err.Error() != tc.message {
+			t.Fatalf("\nexpected: %v\ngot: %v\n", ERROR_RESPONSE, err)
+		}
+	}
+
+}
